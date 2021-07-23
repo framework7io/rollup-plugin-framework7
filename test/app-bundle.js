e400d7a2795a27300762c8ede83f490a3a959190
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-07-23 15:25:41 +0300

    feat: add support for `.f7` files

--- test/app-bundle.js
+++ test/app-bundle.js
@@ -22,7 +22,31 @@ function framework7Component(props) {
     ;
 }
 
-framework7Component.id = '93956a0b65';
+framework7Component.id = '9f70f119f8';
+
+function framework7Component$1(props) {
+  let foo = 'bar';
+  return function ($ctx) {
+      $ctx.$;
+      var $h = $ctx.$h;
+      $ctx.$root;
+      $ctx.$f7;
+      $ctx.$f7route;
+      $ctx.$f7router;
+      $ctx.$theme;
+      $ctx.$update;
+      $ctx.$store;
+
+      return $h`
+  <div>
+    <p>Hello world! ${foo}</p>
+  </div>
+`
+    }
+    ;
+}
+
+framework7Component$1.id = 'fc490c4318';
 
 /** @jsx $jsx */
 var AppTemplateJS = (function (props) {
@@ -30,4 +54,5 @@ var AppTemplateJS = (function (props) {
 });
 
 console.log(framework7Component);
+console.log(framework7Component$1);
 console.log(AppTemplateJS);
--- test/app.js
+++ test/app.js
@@ -1,5 +1,7 @@
 import AppTemplateHTML from './app.f7.html';
+import AppTemplateHTML2 from './app.f7';
 import AppTemplateJS from './app.f7.js';
 
 console.log(AppTemplateHTML);
+console.log(AppTemplateHTML2);
 console.log(AppTemplateJS);
